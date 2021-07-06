f3774f6d3e2c6cd9bf6c1983cf7a01fce1e47217
ABDULLA4G3 <abdulla4g3@sasi.ac.in>
2021-07-06 19:39:18 +0530

    set up all recipe

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>hello world</h1>
+     <h2>welcome to session on recipe</h2>
+    </div>
+  );
+}
+
+export default App;
